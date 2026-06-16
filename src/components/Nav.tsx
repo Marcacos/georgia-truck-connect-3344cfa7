1c843ca759cc819ad2453fd07ce9b38aba53cf5f
Marcacos <mvpfer0@gmail.com>
2026-06-15 21:24:06 -0300

    Bloqueia scroll ao abrir menu (position fixed)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -23,9 +23,17 @@ export function Nav() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  useEffect(() => {
-    document.body.style.overflow = open ? "hidden" : "";
-  }, [open]);
+useEffect(() => {
+  if (open) {
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
+  } else {
+    document.body.style.overflow = "";
+    document.body.style.position = "";
+    document.body.style.width = "";
+  }
+}, [open]);
 
   return (
     <header
